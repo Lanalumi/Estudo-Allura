ee501e88e0396d9fac4adb3d6c2fd679ef495f54
Lanalumi <lanalumi97@gmail.com>
2025-05-05 22:58:11 +0000

    Aulas 1, 2 e 3

---
+++ portfolio/introdução/logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert("Boas-vindas ao jogo do Número secreto!");
+let numerosecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if(numerosecreto == chute) {
+    alert("Você acertou!");
+}
+else{
+    alert("Você errou, tente novamente");
+}
---
+++ portfolio/introdução/logica-js-projeto_inicial/desafio-js-projeto_inicial/app.js
@@ -0,0 +1,13 @@
+alert("Bem-vindas ao nosso site!");
+let nome = "Lua";
+let idade = 25;
+let numerodevendas = 50;
+let saldodisponivel = 1000;
+alert("Erro!Preencha todos os campos!");
+let mensagemDeErro = "Erro!Preencha todos os campos";
+alert(mensagemDeErro);
+let nome1 = prompt("Digite seu nome");
+let idade1 = prompt("Digite sua idade");
+if (idade1 == 18 || idade1 >18){
+    alert("Você pode tirar sua habilitação!");
+}
