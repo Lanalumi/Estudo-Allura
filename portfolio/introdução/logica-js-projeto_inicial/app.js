43c7123bd041c91f51977b7420e31ac3b047c3dd
Lanalumi <lanalumi97@gmail.com>
2025-05-05 22:58:40 +0000

    Adicionei pasta de aula

--- portfolio/introdução/logica-js-projeto_inicial/app.js
+++ portfolio/introdução/logica-js-projeto_inicial/app.js
@@ -1,10 +1,20 @@
 alert("Boas-vindas ao jogo do Número secreto!");
 let numerosecreto = 29;
-let chute = prompt("Escolha um número entre 1 e 30");
+let chute
 
-if(numerosecreto == chute) {
-    alert("Você acertou!");
-}
-else{
-    alert("Você errou, tente novamente");
-}
+while(chute != numerosecreto)
+    {
+    let chute = prompt("Escolha um número entre 1 e 30");
+    if(numerosecreto == chute) {
+    alert(`Você acertou o número secreto ${numerosecreto}`);
+    }
+    else{
+        if (numerosecreto < chute )
+            {
+        alert(`O número secreto é menor que ${chute}`);
+            }
+        else{
+        alert(`O número secreto é maior que ${chute}`);
+            }
+        }
+    }
