55413dd9f41dd189792acc61f5b0d9e5b22bfd5a
Lanalumi <lanalumi97@gmail.com>
2025-05-06 21:34:04 +0000

    Aula 1 react+Js, diretorio inicial, banner.jsx e banner.css

---
+++ React+javaS/organo-app/src/componentes/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner()
+{
+    return (
+        <header className="banner"> 
+        <img  src="/assets/imagens/banner.png" />
+        </header>
+    )
+
+}
+
+export default Banner
