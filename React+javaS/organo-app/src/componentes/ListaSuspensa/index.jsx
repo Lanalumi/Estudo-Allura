8ab7b04c5542243aaa716b3108f6cf4a550037c0
Lanalumi <lanalumi97@gmail.com>
2025-05-07 23:02:38 +0000

    Projeto Organo criação diretórios e arquivos lista suspensa

---
+++ React+javaS/organo-app/src/componentes/ListaSuspensa/index.jsx
@@ -0,0 +1,15 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+    return (
+        <div>
+            <label>{props.label}</label>
+            <select>
+                <option> ???</option>
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
