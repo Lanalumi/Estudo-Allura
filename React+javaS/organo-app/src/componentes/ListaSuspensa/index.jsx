8a7dcb8419c4822b333d72928f5a5fdda6f11c58
Lanalumi <lanalumi97@gmail.com>
2025-05-08 23:13:26 +0000

    Continuação formulário, botão e lista suspensa

--- React+javaS/organo-app/src/componentes/ListaSuspensa/index.jsx
+++ React+javaS/organo-app/src/componentes/ListaSuspensa/index.jsx
@@ -2,11 +2,14 @@ import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
     console.log(props.itens)
+
     return (
-        <div>
+        <div className="lista-suspensa">
             <label>{props.label}</label>
-            <select>
-                <option> oi oi oi banana </option>
+            <select required={props.required}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                    })}     {/*renderização lista simples */}
             </select>
         </div>
     )
