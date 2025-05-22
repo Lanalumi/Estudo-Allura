3c1bcf1a8420457965e56184e8f09c09ec865c85
Lana <lanalumi97@gmail.com>
2025-05-22 18:34:55 -0300

    finalização projeto e fix de digitação

--- React+javaS/organo-app/src/componentes/Formulario/index.jsx
+++ React+javaS/organo-app/src/componentes/Formulario/index.jsx
@@ -1,71 +1,64 @@
-import Botao from '../Botao'
-import CampoTexto from '../CampoTexto'
-import ListaSuspensa from '../ListaSuspensa'
-import './Formulario.css'
-import { useState } from 'react'
+import Botao from "../Botao";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import "./Formulario.css";
+import { useState } from "react";
 
 const Formulario = (props) => {
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault(); //não seguir comportamento padrão
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
+    setNome("");
+    setCargo("");
+    setImagem("");
+    setTime("");
+  };
 
-    
-    const [nome, setNome] = useState('')
-    const [cargo, setCargo] = useState('')
-    const [imagem, setImagem] = useState('')
-    const [time, setTime] = useState('')
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={props.times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
+        <Botao>Criar card</Botao>
+      </form>
+    </section>
+  );
+};
 
-    const aoSalvar = (evento) =>{
-        evento.preventDefault()  //não seguir comportamento padrão
-        props.aoColaboradorCadastrado({
-            nome,
-            cargo,
-            imagem,
-            time
-        })
-        setNome('')
-        setCargo('')
-        setTime('')
-        
-    }
-
-    return (
-        <section className="formulario">
-
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Nome" 
-                    placeholder="Digite seu nome"
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                    />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Cargo" 
-                    placeholder="Digite seu cargo"
-                    valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                    />
-                <CampoTexto 
-                    label="Imagem" 
-                    placeholder="Digite o endereço da imagem"
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}
-                    />
-                <ListaSuspensa 
-                    obrigatorio={true}
-                     label="Time" 
-                     itens={props.times}
-                     valor={time}
-                     aoAlterado={valor => setTime(valor)}
-                     />
-                <Botao>
-                    Criar card 
-                </Botao>
-            </form>
-        </section>
-    )
-
-}
-
-export default Formulario
+export default Formulario;
--- React+javaS/organo-app/src/componentes/Time/time.jsx
+++ React+javaS/organo-app/src/componentes/Time/time.jsx
@@ -1,17 +1,28 @@
-import Colaborador from '../Colaborador/colaborador'
-import './Time.css'
+import Colaborador from "../Colaborador/colaborador";
+import "./Time.css";
 
 const Time = (props) => {
-    const css = {backgroundColor: props.corSecundaria}
+  const css = { backgroundColor: props.corSecundaria };
 
-    return (
-        //renderização condicional
-        props.colaboradores.length > 0 && <section className="time" style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className="colaboradores">
-                {props.colaboradores.map( colaboradores => <Colaborador key={colaboradores.nome} nome={colaboradores.nome} cargo={colaboradores.cargo} imagem={colaboradores.imagem}/>)}
-            </div>
-        </section>
+  return (
+    //renderização condicional
+    props.colaboradores.length > 0 ? (
+      <section className="time" style={css}>
+        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              // key={colaborador.nome}
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    ) : (
+      ""
     )
-}
+  );
+};
 export default Time;
